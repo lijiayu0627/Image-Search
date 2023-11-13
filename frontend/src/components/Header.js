17c3479cf6820d81f31173869d7d28f7f18e2299
lijiayu <lijiayu627x@gmail.com>
2023-11-13 12:13:39 +0800

    eslint and prettier set up

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: '#8F0'
+  backgroundColor: '#8F0',
 };
 
 const Header = (props) => {
-    return (
-        <Navbar style={navbarStyle}>
-            <Container>
-                <Navbar.Brand href='/'>{props.title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar style={navbarStyle}>
+      <Container>
+        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
