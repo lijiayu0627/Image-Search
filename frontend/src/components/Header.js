bf86d161619db3be7f20a96148f173e632eb8f25
lijiayu <lijiayu627x@gmail.com>
2023-11-14 01:02:29 +0800

    added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: '#8F0',
+  backgroundColor: '#eeeeee',
 };
 
 const Header = (props) => {
   return (
     <Navbar style={navbarStyle}>
       <Container>
-        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '64rem', maxHeight: '4rem' }} />
       </Container>
     </Navbar>
   );
