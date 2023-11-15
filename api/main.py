f6ee7f0818c4d148ea9edd4e19e0831b41d591e2
lijiayu <lijiayu627x@gmail.com>
2023-11-15 21:00:01 +0800

    changed api in the frontend and enabled cors in api

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ import os
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -11,8 +12,9 @@ DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
-
+ 
 app = Flask(__name__)
+CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
