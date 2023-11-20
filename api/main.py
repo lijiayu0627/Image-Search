44092016924bf75deaf3c19e3ea8f4d5f4af38d1
lijiayu <lijiayu627x@gmail.com>
2023-11-20 23:17:05 +0800

    single image endpoint with delete method enabled

--- api/main.py
+++ api/main.py
@@ -34,7 +34,7 @@ def new_image():
     data = response.json()
     return data
 
-@app.route("/images", methods=["GET", "POST"])
+@app.route("/images", methods = ["GET", "POST"])
 def images():
     if request.method == "GET":
         # read images from the database
@@ -47,6 +47,18 @@ def images():
         result = images_collection.insert_one(image)
         inserted_id = result.inserted_id 
         return {"inserted_id": inserted_id}
+    
+@app.route("/images/<image_id>", methods = ["DELETE"])
+def image(image_id):
+    if request.method == "DELETE":
+        # delete image from the database
+        result = images_collection.delete_one({"_id":image_id})
+        if not result:
+            return {"error": "Image wasn't deleted. Please try again."}, 500
+
+        if not result.deleted_count:
+            return {"error": "Image not found."}, 404
+        return {"deleted_id": image_id}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
